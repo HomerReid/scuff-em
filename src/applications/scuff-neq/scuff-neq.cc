ecf5126c0c27e6174b8a5df1f014393c9d559392
Homer Reid <homer@homerreid.com>
2013-03-02 14:40:46 -0500

    set --OmegaMin default to 0.001 in scuff-new

--- src/applications/scuff-neq/scuff-neq.cc
+++ src/applications/scuff-neq/scuff-neq.cc
@@ -65,7 +65,7 @@ int main(int argc, char *argv[])
 
   cdouble OmegaVals[MAXFREQ];        int nOmegaVals;
   char *OmegaFile;                   int nOmegaFiles;
-  double OmegaMin=0.0;               int nOmegaMin;
+  double OmegaMin=0.001;             int nOmegaMin;
   double OmegaMax=-1.0;              int nOmegaMax;
 
   char *TempStrings[2*MAXTEMPS];     int nTempStrings;
