4d1fb1da26688e19094d6748594069286c9906f8
Homer Reid <homer@homerreid.com>
2013-03-04 14:42:19 -0500

    bugfixes in scuff-neq

--- src/applications/scuff-neq/CreateSNEQData.cc
+++ src/applications/scuff-neq/CreateSNEQData.cc
@@ -130,7 +130,7 @@ SNEQData *CreateSNEQData(char *GeoFile, char *TransFile,
   bool *NeedMatrix=SNEQD->NeedMatrix;
   memset(NeedMatrix, 0, SCUFF_NUM_OMATRICES*sizeof(bool));
   NeedMatrix[SCUFF_OMATRIX_OVERLAP] = 0;
-  NeedMatrix[SCUFF_OMATRIX_POWER  ] = QuantityFlags & QFLAG_POWER;
+  NeedMatrix[SCUFF_OMATRIX_POWER  ] = true;
   NeedMatrix[SCUFF_OMATRIX_XFORCE ] = QuantityFlags & QFLAG_XFORCE;
   NeedMatrix[SCUFF_OMATRIX_YFORCE ] = QuantityFlags & QFLAG_YFORCE;
   NeedMatrix[SCUFF_OMATRIX_ZFORCE ] = QuantityFlags & QFLAG_ZFORCE;
