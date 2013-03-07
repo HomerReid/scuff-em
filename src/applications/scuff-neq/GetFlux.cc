e5be399a0aeb6fc7643e5531415901efec536973
Homer Reid <homer@homerreid.com>
2013-03-07 01:11:04 -0500

    updated scuff-neq to use new SMatrix::GetRow() routine

--- src/applications/scuff-neq/GetFlux.cc
+++ src/applications/scuff-neq/GetFlux.cc
@@ -315,22 +315,22 @@ void GetFlux(SNEQData *SNEQD, cdouble Omega, double *FI)
          int nq=0;
          if ( QuantityFlags & QFLAG_POWER )
           { int i = GetIndex(SNEQD, nt, nss, nsd, nq++);
-            FI[i] = GetTrace(SNEQD, QINDEX_POWER, nss, nsd, f);
+            FI[i] = GetTrace(SNEQD, QINDEX_POWER, nss, nsd);
             fprintf(f,"%.8e ",FI[i]);
           };
          if ( QuantityFlags & QFLAG_XFORCE )
           { int i = GetIndex(SNEQD, nt, nss, nsd, nq++);
-            FI[i] = GetTrace(SNEQD, QINDEX_XFORCE, nss, nsd, f);
+            FI[i] = GetTrace(SNEQD, QINDEX_XFORCE, nss, nsd);
             fprintf(f,"%.8e ",FI[i]);
           }
          if ( QuantityFlags & QFLAG_YFORCE )
           { int i = GetIndex(SNEQD, nt, nss, nsd, nq++);
-            FI[i] = GetTrace(SNEQD, QINDEX_YFORCE, nss, nsd, f);
+            FI[i] = GetTrace(SNEQD, QINDEX_YFORCE, nss, nsd);
             fprintf(f,"%.8e ",FI[i]);
           }
          if ( QuantityFlags & QFLAG_ZFORCE )
           { int i = GetIndex(SNEQD, nt, nss, nsd, nq++);
-            FI[i] = GetTrace(SNEQD, QINDEX_ZFORCE, nss, nsd, f);
+            FI[i] = GetTrace(SNEQD, QINDEX_ZFORCE, nss, nsd);
             fprintf(f,"%.8e ",FI[i]);
           };
 
